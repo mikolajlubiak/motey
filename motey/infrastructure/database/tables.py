931c86c79cd13a66ddaeb5574cb09ee949757e4e
Mikołaj Lubiak <lubiak@proton.me>
2023-07-03 17:37:12 +0200

    Deleted nullable argument
    Nullable isn't needed since we have defined default value.

--- motey/infrastructure/database/tables.py
+++ motey/infrastructure/database/tables.py
@@ -28,7 +28,7 @@ emotes = Table(
     Column('emote_id', BIGINT, primary_key=True, autoincrement=True),
     Column('name', VARCHAR(12), nullable=False, unique=True,),
     Column('location', VARCHAR(128), nullable=False, unique=True),
-    Column('times_used', BIGINT, nullable=False, default=0),
+    Column('times_used', BIGINT, default=0),
     Column('created_at', DATETIME, server_default=func.current_timestamp()),
     Column('user_id', BIGINT, ForeignKey('users.user_id'))
 )
