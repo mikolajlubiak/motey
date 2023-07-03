67ff820b1a92d6badb639ab073e3b9b8ecfde8d3
Mikołaj Lubiak <lubiak@proton.me>
2023-07-03 21:30:02 +0200

    Fixed login bug Login query return multiple results while we were using one_or_none (One or no results)

--- motey/infrastructure/http/views.py
+++ motey/infrastructure/http/views.py
@@ -86,7 +86,7 @@ async def login(request: web.Request):
     with request.app['db'].connect() as connection:
         statement = select(users)\
             .where(login==login, password==password)
-        userId = connection.scalars(statement).one_or_none()
+        userId = connection.scalars(statement).all()
         # TODO
         # if userId:
             # Successful login
