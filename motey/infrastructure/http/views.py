8277d2a049157973d3e67dd278ff2484256069fd
Mikołaj Lubiak <lubiak@proton.me>
2023-07-09 08:06:21 +0200

    Add emote author and user is added to the database if he/she doesnt already existin the database

--- motey/infrastructure/http/views.py
+++ motey/infrastructure/http/views.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import Session
 
 from motey.infrastructure.database.storage import EmoteStorage
 from motey.infrastructure.filesystem import EmoteFileWriter
+from motey.infrastructure.database.tables import User, Server, Emote
 
 from motey.infrastructure.config import Config
 
@@ -15,8 +16,8 @@ routes = web.RouteTableDef()
 
 @aiohttp_jinja2.template('list.html')
 async def list_emotes(request: web.Request):
-    with Session(request.app['db']) as session:
-        return EmoteStorage(session).fetch_all_emotes()
+    with Session(request.app['db']) as db_session:
+        return EmoteStorage(db_session).fetch_all_emotes()
 
 
 @aiohttp_jinja2.template('index.html')
@@ -29,6 +30,7 @@ async def upload(request: web.Request):
     data = await request.post()
     emote = data['emote']
     emote_name = data['emotename']
+    session['discord_id'] = user_data['id']
     if not emote_name:
         return {'error_message': 'Please enter emote name'}
 
@@ -38,11 +40,13 @@ async def upload(request: web.Request):
     else:
         return {'error_message': 'File extension invalid'}
 
-    with Session(request.app['db']) as session:
-        emote_storage = EmoteStorage(session)
+    with Session(request.app['db']) as db_session:
+        stmt = select(User).where(User.discord_id==session['discord_id'])
+        author = db_session.scalars(stmt).one()
+        emote_storage = EmoteStorage(db_session)
         if emote_storage.emote_exists(emote_name):
             return {'error_message': 'Emote with this name already exists'}
-        emote_storage.add_emote(emote_name, str(file_writer.path), author) # `author` needs to be object of User class that is the author of this Emote
+        emote_storage.add_emote(emote_name, str(file_writer.path), author)
 
     raise web.HTTPFound(location='/')
 
@@ -50,6 +54,7 @@ async def upload(request: web.Request):
 @routes.get('/process_oauth')
 async def process_oauth(request: web.Request):
     code = request.rel_url.query.get("code", "")
+    session = await aiohttp_session.get_session(request)
     if not code:
         return {'error_message': 'There is no oauth code'}
     payload = {
@@ -60,20 +65,24 @@ async def process_oauth(request: web.Request):
             "redirect_uri": Config.redirect_url,
             "scope": "identify%20guilds"
         }
-    async with aiohttp.ClientSession() as session:
-        async with session.post("https://discord.com/api/oauth2/token", data=payload) as response:
+    async with aiohttp.ClientSession() as client_session:
+        async with client_session.post("https://discord.com/api/oauth2/token", data=payload) as response:
             auth_token_data = await response.json()
     access_token = auth_token_data["access_token"]
     header = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/x-www-form-urlencoded"
     }  
-    async with aiohttp.ClientSession(headers=header) as session:
-        async with session.get("https://discord.com/api/users/@me") as response:
+    async with aiohttp.ClientSession(headers=header) as client_session:
+        async with client_session.get("https://discord.com/api/users/@me") as response:
             user_data = await response.json()
-            session = await aiohttp_session.get_session(request)
             session['discord_id'] = user_data['id']
-    async with aiohttp.ClientSession(headers=header) as session:
-        async with session.get("https://discord.com/api/users/@me/guilds") as response:
+    async with aiohttp.ClientSession(headers=header) as client_session:
+        async with client_session.get("https://discord.com/api/users/@me/guilds") as response:
             guilds = await response.json()
+    with Session(request.app['db']) as db_session:
+        if not db_session.query(exists().where(User.discord_id == session['discord_id'])).scalar():
+            user = User(discord_id=session['discord_id'])
+            db_session.add(user)
+            db_session.commit()
     raise web.HTTPFound(location='/')
