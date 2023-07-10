105950f0615cb4640d89fa91810a0f1f8e5b3a95
Mikołaj Lubiak <lubiak@proton.me>
2023-07-10 06:31:41 +0000

    Added favicon

--- motey/infrastructure/http/routing.py
+++ motey/infrastructure/http/routing.py
@@ -1,8 +1,8 @@
 from aiohttp.web import Application
+from motey.infrastructure.config import Config
 
 from motey.infrastructure.http.views import list_emotes, index, upload, process_oauth
 from motey.infrastructure.config import Config
-
 
 def setup_routes(app: Application, config: Config = Config()) -> None:
     app.router.add_static(str(config.emotes_dir), config.emotes_dir)
@@ -10,3 +10,6 @@ def setup_routes(app: Application, config: Config = Config()) -> None:
     app.router.add_get('/', index)
     app.router.add_post('/upload', upload)
     app.router.add_get('/process_oauth', process_oauth)
+    app.router.add_static(
+        '/static/', path=(config.static_files_dir), name='static'
+    )
