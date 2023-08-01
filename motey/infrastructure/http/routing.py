2b58390d9af7ecddf6bcb03fa2e9a40f06078d37
Mikołaj Lubiak <lubiak@proton.me>
2023-08-01 14:29:21 +0200

    Fix `POST is already registered`

--- motey/infrastructure/http/routing.py
+++ motey/infrastructure/http/routing.py
@@ -10,7 +10,7 @@ def setup_routes(app: Application, config: Config = Config()) -> None:
     app.router.add_get('/list', list_emotes)
     app.router.add_get('/', index)
     app.router.add_post('/upload', process_upload)
-    app.router.add_post('/upload', upload)
+    app.router.add_get('/upload', upload)
     app.router.add_get('/process_oauth', process_oauth)
     app.router.add_static('/static/', path=(config.static_files_dir), name='static')
     app.router.add_get('/login', lambda _: web.HTTPFound(location=config.auth_start_url))
