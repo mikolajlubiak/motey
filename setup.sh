46ebcae0945977e64a356295dd9c44a4a0980519
Mikołaj Lubiak <lubiak@proton.me>
2024-11-03 13:27:55 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Python enviroment
 virtualenv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade setuptools
 
+# Database
+sudo docker-compose up -d
 ./alembic.sh
