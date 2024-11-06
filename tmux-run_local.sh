65e352f05c08fe1d67e653ad0a4cdd85a8ebddcf
paczek654321 <paczek654321@gmail.com>
2024-11-06 20:00:13 +0100

    Added a working local startup script

---
+++ tmux-run_local.sh
@@ -0,0 +1,14 @@
+unset PROMPT_COMMAND
+
+tmux set -g pane-border-status top
+tmux set -g mouse on
+tmux set -g pane-border-format "#{pane_title}"
+
+tmux split-window -fh "tmux select-pane -T \"Web Backend\"; ./start_web-run_local.sh"
+
+tmux split-window -fh "tmux select-pane -T \"Discord Bot\"; ./bot.sh"
+
+tmux split-window -fv -l "2"\
+    "tmux select-pane -T \"Web URL\";\
+    source .env;\
+    echo \"http://\${HTTP_HOST}:\${HTTP_PORT}\" | less"
