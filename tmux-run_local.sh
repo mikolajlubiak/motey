9de42c3c7b81076ab0cd39ca2e7f71bbc80eb230
paczek654321 <paczek654321@gmail.com>
2024-11-06 20:02:34 +0100

    Removed an unnecessary line from the local startup script

--- tmux-run_local.sh
+++ tmux-run_local.sh
@@ -4,8 +4,6 @@ tmux set -g pane-border-status top
 tmux set -g mouse on
 tmux set -g pane-border-format "#{pane_title}"
 
-br="-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
-
 tmux split-window -fh "tmux select-pane -T \"Web Backend\"; ./start_web-run_local.sh"
 
 tmux split-window -fh "tmux select-pane -T \"Discord Bot\"; ./bot.sh"
