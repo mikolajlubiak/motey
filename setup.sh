85fca3a495fb383f1257b4832584ab46d2c99018
paczek654321 <paczek654321@gmail.com>
2024-11-06 21:06:02 +0100

    Improved the setup script.

--- setup.sh
+++ setup.sh
@@ -8,5 +8,25 @@ pip install --upgrade setuptools
 
 # Database
 sudo docker-compose up -d
-sleep 1 # Wait for the database to launch inside the container
-./alembic.sh
+
+success=0
+try=0
+
+while [ $success = 0 ]; do
+    try=$(($try+1))
+    ./alembic.sh && success=1
+
+    if [ $success = 0 ]; then
+        if (($try > 9)); then
+            success=2
+            echo "10 failed attempts at connecting to the database, aborting."
+        else
+            echo "Connecting to the database was unsuccessful, retrying in 1 second. Current attempt: ${try}"
+            sleep 1
+        fi
+    fi
+
+    if [ $success = 1 ]; then
+        echo "Connecting to the database was successful!"
+    fi
+done
